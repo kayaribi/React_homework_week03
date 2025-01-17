389711b8b0a664a09bba0253e57007b553788aba
Kaya <kaya_chen@outlook.com>
2025-01-18 01:33:31 +0800

    變更路徑

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/React_homework_week02/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/React_homework_week03/' : '/',
   plugins: [react()],
 })
